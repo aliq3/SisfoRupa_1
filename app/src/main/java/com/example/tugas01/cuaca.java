40ce9dd4cdf68e1260216ed1d415ab176550f452
Muhammad Nurkhaliq <muhammadkhaliq@student.untan.ac.id>
2019-09-19 21:55:38 +0700

    progresss

---
+++ app/src/main/java/com/example/tugas01/cuaca.java
@@ -0,0 +1,14 @@
+package com.example.tugas01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cuaca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuaca);
+    }
+}
